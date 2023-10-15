c6bd3bc6210a7226a3d5788a46d36bc8b8755f42
Nicocchi <nicocchilover@gmail.com>
2023-10-15 02:22:22 -0400

    simple read from disk

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -66,25 +66,151 @@ puts:
 main:
 
   ; setup data segments
-  mov ax, 0     ; can't write to ds/es directly
+  mov ax, 0             ; can't write to ds/es directly
   mov ds, ax
 
-  ; setup stack - set stack segment to 0 and a stack pointer to the beginning of the
-  ; program.
+  ; setup stack - set stack segment to 0 and a stack pointer to the beginning of the program
   mov ss, ax
-  mov sp, 0x7C00  ; stack grows downwards from where we are loaded in memory
+  mov sp, 0x7C00        ; stack grows downwards from where we are loaded in memory
 
-  ; print message
+  ; read something from floppy disk
+  ; BIOS should set DL to drive number
+  mov [ebr_drive_number], dl
+
+  mov ax, 1             ; LBA=1, second sector from disk
+  mov cl, 1             ; 1 sector to read
+  mov bx, 0x7E00        ; data should be after the bootloader
+  call disk_read
+
   mov si, msg_hello
   call puts
 
+  cli
   hlt
 
-; in certain cases the CPU can start executing again so run an infinite loop
-.halt:
-  jmp .halt
+;
+; Error handlers
+;
+floppy_error:
+  mov si, msg_read_failed
+  call puts
+  jmp wait_key_and_reboot
 
-msg_hello: db 'Hello world!', 0
+wait_key_and_reboot:
+  mov ah, 0
+  int 16h                         ; wait for keypress
+  jmp 0FFFFh:0                    ; jump to beginning of BIOS, should reboot
+
+.halt:
+  cli                             ; disable interrupts, this way the CPU can't get out of "halt" state
+  hlt
+
+; Disk routines
+
+; Converts an LBA address to a CHS address
+; Parameters:
+;   - ax: LBA address
+; Returns:
+;   - cx [bits 0-5]: sector number
+;   - cx [bits 6-15]: cylinder
+;   - dh: head
+;
+
+lba_to_chs:
+
+  push ax
+  push dx
+
+  xor dx, dx                          ; dx = 0
+  div word [bdb_sectors_per_track]    ; ax = LBA / SectorsPerTrack
+                                      ; dx = LBA % SectorsPerTrack
+  inc dx                              ; dx = (LBA % SectorsPerTrack + 1) = sector
+  mov cx, dx                          ; cx = sector
+
+  xor dx, dx                          ; dx = 0
+  div word [bdb_heads]                ; ax = (LBA / SectorsPerTrack) / Heads = cylinder
+                                      ; dx = (LBA / SectorsPerTrack) % Heads = head
+
+  mov dh, dl                          ; dh = head
+  mov ch, al                          ; ch = cylinder (lower 8 bits)
+  shl ah, 6
+  or cl, ah                           ; put upper 2 bits of cylinder in CL
+
+  pop ax
+  mov dl, al                          ; restore DL
+  pop ax
+  ret
+
+
+;
+; Reads sectors from a disk
+; Parameters:
+;   - ax: LBA Address
+;   - cl: number of sectors to read (up to 128)
+;   - dl: drive number
+;   - es:bx: memory address where to store read data
+;
+
+disk_read:
+
+  push ax                             ; save registers we will modify
+  push bx
+  push cx
+  push dx
+  push di
+
+  push cx                             ; temporarliy save CL (number of sectors to read)
+  call lba_to_chs                     ; compute CHS
+  pop ax                              ; AL = number of sectors to read
+
+  mov ah, 02h
+  mov di, 3                           ; retry count - Floppy's are unreliable IRL so check x amount of times
+
+.retry:
+  pusha                               ; save all registers, we don't know what bios modifiers
+  stc                                 ; set carry flag, some BIOS'es don't set it
+  int 13h                             ; carry flag cleared = success
+  jnc .done                           ; jump if carry not set
+
+  ; failed
+  popa
+  call disk_reset
+
+  dec di
+  test di, di
+  jnz .retry
+
+.fail:
+  ; after attempts are exhaused
+  jmp floppy_error
+
+.done:
+  popa
+
+  pop di                              ; restore registers modified
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+  ret
+  
+;
+; Resets disk controller
+; Parameters:
+;   - dl: drive number
+;
+disk_reset:
+  pusha
+  mov ah, 0
+  stc
+  int 13h
+  jc floppy_error
+  popa
+  ret
+
+
+msg_hello:              db 'Hello world!', ENDL, 0
+msg_read_failed:        db 'Failed to read from disk', ENDL, 0
 
 ; fill up 512 bytes
 times 510-($-$$) db 0 ;$-$$ is the size of the prgram so far in bytes
